270255ab6e1d4cbaf11ecfbce486ab440109c0b7
Kristian Gjertsen <kristiangjertsen@gmail.com>
2026-01-21 18:24:36 +0100

    Import unused rettet

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Header from "../../components/pageSections/Header";
 import Footer from "../../components/pageSections/Footer";
