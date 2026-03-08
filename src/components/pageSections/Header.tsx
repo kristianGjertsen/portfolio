31e2f6a055f350a82378baadfd9227d982de4fb8
Kristian Gjertsen <kristiangjertsen@gmail.com>
2026-03-08 20:19:24 +0100

    Header fikset, ikke lenger noe transparent

--- src/components/pageSections/Header.tsx
+++ src/components/pageSections/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between  px-6 py-4 text-ink backdrop-blur shadow-lg">
+    <header className="sticky top-0 z-50 flex items-center justify-between bg-inherit px-6 py-4 text-ink shadow-lg backdrop-blur-none">
       {/* Venstre side */}
       <div className="flex items-center gap-10">
         <Link
